9ff43acfc0d15989b27bf365eca177cff1d8a843
satojkovic <satojkovic@gmail.com>
2014-05-25 01:48:39 +0900

    Add sample script

---
+++ hue_sample.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+from phue import Bridge
+from pit import Pit
+import sys
+
+
+def main():
+    conf = Pit.get(
+        'hue', {'require': {'ip_addr': 'IP Address of Hue'}}
+    )
+
+    b = Bridge(conf['ip_addr'])
+
+    lights = b.lights
+    for l in lights:
+        print l.name
+
+if __name__ == '__main__':
+    main()
